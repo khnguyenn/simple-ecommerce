4e17dfdde65afae56fc9080c96cd93e07d74e336
khnguyenn <khoinguyennn231@gmail.com>
2025-04-22 00:38:03 +1000

    Half Project Done

---
+++ ecommerce/core/forms.py
@@ -0,0 +1,39 @@
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'w-full py-4 px-6 rounded-xl',
+        'placeholder': 'Your username'
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full py-4 px-6 rounded-xl',
+        'placeholder': 'Your password'
+    }))
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User 
+        fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'w-full py-4 px-6 rounded-xl',
+        'placeholder': 'Your username'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'w-full py-4 px-6 rounded-xl',
+        'placeholder': 'Your email address'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full py-4 px-6 rounded-xl',
+        'placeholder': 'Your password'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full py-4 px-6 rounded-xl',
+        'placeholder': 'Repeat your password'
+    }))
